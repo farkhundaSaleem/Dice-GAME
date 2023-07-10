1fc416dd896393014bc790665b458c0101ad69e0
Farkhunda Saleem <96863402+farkhundaSaleem@users.noreply.github.com>
2023-07-10 23:25:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+
+var images1 = "images/dice"+ randomNumber1 +".png";
+var images2 = "images/dice"+ randomNumber2 +".png";
+ 
+document.querySelector(".dice .img1").setAttribute("src",images1);
+document.querySelector(".dice .img2").setAttribute("src",images2);
+
+
+
+if (images1>images2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if(images2>images1)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins🚩!";
+} 
+else if(images2===images1)
+{
+    document.querySelector("h1").innerHTML="Draw🏁";
+} 
